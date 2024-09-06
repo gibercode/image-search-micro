5febf1ca155626dd85b13cdd8d27a5d36fc44e5f
gibercode <gibersonlara@Gibersons-MacBook-Pro.local>
2024-09-05 20:42:16 -0400

    path added

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const Dotenv = require("dotenv-webpack");
+const path = require("path");
 
 module.exports = {
   entry: "./src/index.tsx",
