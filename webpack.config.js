d3bb8e495df8e99ca92de69e3f755a20d120e6ee
gibercode <gibersonlara@Gibersons-MacBook-Pro.local>
2024-09-09 11:17:50 -0400

    fileName changed again

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
   output: {
     filename: "[name].[contenthash].js",
     chunkFilename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
     publicPath: "auto",
   },
   resolve: {
